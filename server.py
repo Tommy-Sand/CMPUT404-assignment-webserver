436f70ef062873188db112175a38695c521a2483
Tommy-Sand <rosansandanasamy@gmail.com>
2023-01-11 08:09:08 -0700

    Sends 405 on improper requests

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import socketserver
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # 
-#     http://www.apache.org/licenses/LICENSE-2.0
+#	 http://www.apache.org/licenses/LICENSE-2.0
 # 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,21 +26,55 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+HTTP_200 = """HTTP/1.1 200 OK\n\n"""
+HTTP_301 = "HTTP/1.1 301 MOVED PERMANENTLY\n"
+HTTP_400 = "HTTP/1.1 400 BAD REQUEST\n"
+HTTP_404 = "HTTP/1.1 404 NOT FOUND\n"
+HTTP_405 = "HTTP/1.1 405 METHOD NOT ALLOWED\n"
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+	
+	def handle(self):
+		self.data = self.request.recv(1024).strip()
+		print ("Got a request of: %s\n" % self.data)
+		if(len(self.data.split(b"\n")) == 0): 
+			#Throw 400 error
+			self.handle_400()
+			return
+		start_line = self.data.split(b"\n", 1)[0].strip()
+		if(start_line.startswith(b"GET")):
+			#Can proceed
+			path = "./www"
+			print("Can proceed")
+			#absolute path
+			#complete URL
+			f1 = open("./www/index.html", "r")
+			self.request.sendall(bytearray(HTTP_200 + f1.read(), 'utf-8'))
+		elif(start_line.startswith((b"HEAD", b"POST", b"PUT", b"DELETE", b"CONNECT", b"OPTIONS", b"TRACE", b"PATCH"))):
+			#Throw 405 error
+			self.handle_405()
+		else:
+			#Throw 400 error
+			self.handle_400()
+		#Make sure it's a proper url
+			#If the url is http://127.0.0.1:8080/
+		#self.request.sendall(bytearray(HTTP_200,'utf-8'))
+
+	def handle_400(self):
+		ERROR = "\nBad Request\n"
+		self.request.sendall(bytearray(HTTP_400 + ERROR, 'utf-8'))
+
+	def handle_405(self):
+		ERROR = "\nAllow: GET\n"
+		self.request.sendall(bytearray(HTTP_405 + ERROR, 'utf-8'))
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
+	HOST, PORT = "localhost", 8080
 
-    socketserver.TCPServer.allow_reuse_address = True
-    # Create the server, binding to localhost on port 8080
-    server = socketserver.TCPServer((HOST, PORT), MyWebServer)
+	socketserver.TCPServer.allow_reuse_address = True
+	# Create the server, binding to localhost on port 8080
+	server = socketserver.TCPServer((HOST, PORT), MyWebServer)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+	# Activate the server; this will keep running until you
+	# interrupt the program with Ctrl-C
+	server.serve_forever()
